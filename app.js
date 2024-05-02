af268e88e20780e880495df369c2db084610242c
cabrel20 <yannfonkoue@gmail.com>
2024-05-02 13:29:39 +0100

    working on sign up feature

--- app.js
+++ app.js
@@ -1,4 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongodbStrore = require("connect-mongodb-session")(session);
+
+const MONGODB_URI =
+  "mongodb+srv://yann:loulou237@cluster0.lajrlny.mongodb.net/taskflow_db?retryWrites=true&w=majority&appName=Cluster0";
 
 const app = express();
+const store = new MongodbStrore({ uri: MONGODB_URI, collection: "sessions" });
+
+const errorController = require("./controllers/error-controller");
+const authRoutes = require("./routes/auth-routes");
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: "my-secret",
+    saveUninitialized: false,
+    resave: false,
+    store: store,
+  })
+);
+
+app.use("/auth", authRoutes);
+app.use(errorController.get404);
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    console.log("database connection");
+    app.listen(4000);
+  })
+  .catch((err) => console.log(err));
